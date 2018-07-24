04e3be1646412a01477b2d9a63ee61b59f091365
John Oerter <joerter@gmail.com>
2018-07-23 22:42:11 -0500

    Add ticks to count down the current goal

--- Main.elm
+++ Main.elm
@@ -3,11 +3,13 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 import Time exposing (..)
 
--- time is always in minutes because the tasks shouldn't
--- be longer than 60 minutes
-main : Program Never Model Msg
 main =
-    Html.beginnerProgram {model = model, view = view, update = update}
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 type alias Model =
     { nextGoal : String
@@ -16,29 +18,38 @@ type alias Model =
     , currentTime : Time
     }
 
-model : Model
-model =
+initialModel : Model
+initialModel =
     Model "" (inSeconds 0) "" (inSeconds 0)
+
+init : (Model, Cmd Msg)
+init = (initialModel, Cmd.none)
 
 type Msg
     = NextGoalInput String
     | NextTimeInput String
     | Start
+    | Tick Time
 
 toTimeOrDefault : String -> Time -> Time
 toTimeOrDefault string default =
     String.toFloat string |> Result.toMaybe |> Maybe.withDefault default |> inSeconds
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         NextGoalInput nextGoal ->
-            { model | nextGoal = nextGoal }
+            ({ model | nextGoal = nextGoal }, Cmd.none)
         NextTimeInput nextTime ->
-            { model | nextTime = toTimeOrDefault nextTime 0 }
+            ({ model | nextTime = toTimeOrDefault nextTime 0 }, Cmd.none)
         Start ->
-            { model | currentGoal = model.nextGoal, currentTime = model.nextTime }
+            ({ model | currentGoal = model.nextGoal, currentTime = model.nextTime }, Cmd.none)
+        Tick newTime ->
+            ({ model | currentTime = model.currentTime - 1}, Cmd.none)
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every second Tick
 
 view : Model -> Html Msg
 view model =
